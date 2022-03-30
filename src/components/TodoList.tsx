7336420d3de766ae46cd7c4f686aeab04077b86d
Alex J. Lee <alexjlee.dev@gmail.com>
2022-03-30 14:12:58 +0900

    feat: add kanban board

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import '../styles/style.scss';
 import TodoItem from './TodoItem';
 import { Todo } from '../models/todo';
@@ -9,13 +9,50 @@ interface Props {
 }
 
 const TodoList: React.FC<Props> = ({ todos, setTodos }: Props) => {
+  // return (
+  //   <div className='todo_list'>
+  //     {todos.map((todo) =>
+  //       <TodoItem todo={todo} key={todo.id} todos={todos} setTodos={setTodos}/>
+  //     )}
+  //   </div>
+  // )
   return (
-    <div className='todo_list'>
-      {todos.map((todo) => 
-        <TodoItem todo={todo} key={todo.id} todos={todos} setTodos={setTodos}/>
-      )}
+    <div className='container'>
+      <div className='todos inbox'>
+        <span className='todos__heading'>Inbox</span>
+        {todos.map((todo) => (
+          <TodoItem
+            todo={todo}
+            key={todo.id}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
+      <div className='todos in_progress'>
+      <span className='todos__heading'>In Progress</span>
+      {todos.map((todo) => (
+          <TodoItem
+            todo={todo}
+            key={todo.id}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
+      <div className='todos completed'>
+      <span className='todos__heading'>Completed</span>
+      {todos.map((todo) => (
+          <TodoItem
+            todo={todo}
+            key={todo.id}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TodoList
+export default TodoList;
