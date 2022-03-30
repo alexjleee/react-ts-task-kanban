617a1eda4184390585b1464bdcab044278c3d371
Alex J. Lee <alexjlee.dev@gmail.com>
2022-03-30 16:53:11 +0900

    update: change todo item structure
    - remove edit icon and implement click-to-edit
    - implement custom checkbox
    - apply appropriate tags to elements (button instead of span)

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import '../styles/style.scss';
 import { Todo } from '../models/todo';
-import { FiEdit, FiCheckSquare, FiTrash2 } from 'react-icons/fi';
+import { FiX } from 'react-icons/fi';
 
 interface Props {
   todo: Todo;
@@ -33,37 +33,51 @@ const TodoItem: React.FC<Props> = ({ todo, todos, setTodos }) => {
 
   const handleEdit = (e: React.FormEvent, id: number) => {
     e.preventDefault();
-    setTodos(todos.map((todo) => (todo.id === id) ? {...todo, todo: editTodo} : todo))
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, todo: editTodo } : todo))
+    );
     setEdit(false);
-  }
+  };
+
+  const handleClickToEdit = () => {
+    if (!edit && !todo.isDone) {
+      setEdit(!edit);
+    }
+  };
 
   return (
     <form className='edit-form' onSubmit={(e) => handleEdit(e, todo.id)}>
+      <input
+        type='checkbox'
+        className='invisiblechekbox'
+        checked={todo.isDone}
+        onChange={() => handleDone(todo.id)}
+      />
+      <span className='checkmark'></span>
       {edit ? (
-        <input type='text' className='textinput' ref={inputRef} value={editTodo} onChange={(e) => setEditTodo(e.target.value)}/>
-      ) : todo.isDone ? (
-        <s className='textcontent -completed'>{todo.todo}</s>
+        <input
+          type='text'
+          className='cteinput'
+          ref={inputRef}
+          value={editTodo}
+          onChange={(e) => setEditTodo(e.target.value)}
+          onBlur={(e) => handleEdit(e, todo.id)}
+        />
       ) : (
-        <span className='textcontent'>{todo.todo}</span>
-      )}
-      <div>
-        <span className='iconbtn' onClick={() => handleDone(todo.id)}>
-          <FiCheckSquare />
-        </span>
         <span
-          className='iconbtn'
-          onClick={() => {
-            if (!edit && !todo.isDone) {
-              setEdit(!edit);
-            }
-          }}
+          className={'ctetext' + todo.isDone ? ' -completed' : ''}
+          onClick={handleClickToEdit}
         >
-          <FiEdit />
+          {todo.todo}
         </span>
-        <span className='iconbtn' onClick={() => handleDelete(todo.id)}>
-          <FiTrash2 />
-        </span>
-      </div>
+      )}
+      <button
+        type='button'
+        className='deletebtn'
+        onClick={() => handleDelete(todo.id)}
+      >
+        <FiX />
+      </button>
     </form>
   );
 };
