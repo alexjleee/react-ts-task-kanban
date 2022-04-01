d082b5552f4a8a8ae3f53fa17b643bb70f323fd4
Alex J. Lee <alexjlee.dev@gmail.com>
2022-04-02 01:54:09 +0900

    chore: remove console.log

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,6 @@ const App: React.FC = () => {
   };
 
   const onDragEnd = (result: DropResult) => {
-    console.log(result);
     const { source, destination } = result;
     if (!destination) return;
     if (source.droppableId === destination.droppableId && source.index === destination.index) return;
